6937b5db83006f72e1995846f366a57322dc9ebb
Thang Hoang Van <thang@mainwp.com>
2014-08-13 22:04:30 +0700

    Add Documentation and Support Forum link into Extensions page

--- view/MainWPExtensionsView.view.php
+++ view/MainWPExtensionsView.view.php
@@ -190,7 +190,12 @@ class MainWPExtensionsView
                             </td>
                         </tr>
                         <tr class="mainwp-extensions-extra mainwp-extension-description"><td colspan="3"><?php echo preg_replace('/\<cite\>.*\<\/cite\>/', '', $extension['description']); ?></td></tr>
-                        <tr class="mainwp-extensions-extra"><td colspan="3"><br/><?php printf(__('By %s', 'mainwp'), $extension['author']); ?></td></tr>
+                        <tr class="mainwp-extensions-extra">
+                            <td colspan="3"><br/>
+                                <?php printf(__('By %s', 'mainwp'), $extension['author']); ?>
+                                <?php echo (isset($extension['DocumentationURI']) && !empty($extension['DocumentationURI'])) ? ' | <a href="' . $extension['DocumentationURI'] . '" title="' . __("Documentation", "mainwp") . '">' . __("Documentation", "mainwp") . '</a>' : ""; ?>
+                                <?php echo (isset($extension['SupportForumURI']) && !empty($extension['SupportForumURI'])) ? ' | <a href="' . $extension['SupportForumURI'] . '" title="' . __("Support Forum", "mainwp") . '">' . __("Support Forum", "mainwp") . '</a>' : ""; ?>
+                            </td></tr>
                     </table>
                 </td>
             </table>

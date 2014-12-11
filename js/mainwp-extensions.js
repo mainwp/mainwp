4046a5d155ed38dfd1631291860dee3aad4ddce0
Thang Hoang Van <thang@mainwp.com>
2014-12-11 21:23:11 +0700

    Update message

--- js/mainwp-extensions.js
+++ js/mainwp-extensions.js
@@ -517,7 +517,7 @@ mainwp_extension_bulk_install = function() {
 }
 
 mainwp_extension_bulk_install_done = function() {
-    jQuery('.mainwp_extension_installing').append('<div class="mainwp_info-box">' + __("Install finished.") + '</div><p><a href="admin.php?page=Extensions" title="Extensions page">' + __("Return to the Extensions page to Insert API Keys") + '</a></p>');
+    jQuery('.mainwp_extension_installing').append('<div class="mainwp_info-box">' + __("Install finished.") + '</div><div class="mainwp_info-box">' + __('Refreshing the page for Step 3 "Add API Keys" in 5 seconds... if refresh fails please click <a href="admin.php?page=Extensions" title="Extensions page">here</a>.') + '</div>');
     setTimeout(function ()
     {
       location.href = 'admin.php?page=Extensions'; 

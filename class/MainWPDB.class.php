31e4e31e0386877308eab60e35a10d736c333e61
Thang Hoang Van <thang@mainwp.com>
2015-01-13 22:14:41 +0700

    Update to fix warning of GA extension

--- class/MainWPDB.class.php
+++ class/MainWPDB.class.php
@@ -1211,7 +1211,7 @@ class MainWPDB
                 $wpdb->query('DELETE FROM ' . $this->tableName('wp_group') . ' WHERE wpid=' . $websiteid);
                 //Remove GA stats
                 $showErrors = $wpdb->hide_errors();
-                $wpdb->query('DELETE FROM ' . $this->tableName('wp_ga') . ' WHERE wpid=' . $websiteid);
+                do_action('mainwp_ga_delete_site', $websiteid);                
                 if ($showErrors) $wpdb->show_errors();
                 //add groups with groupnames
                 foreach ($groupnames as $groupname)

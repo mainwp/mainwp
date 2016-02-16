9b09ac732f6e4cc2b8e0a8e2e87924c9f012471d
ruben- <ruben.schollaert@verticon.be>
2016-02-16 19:45:40 +0100

    fix for issue #31

--- cron/bootstrap.php
+++ cron/bootstrap.php
@@ -6,5 +6,28 @@ $mem =  '512M';
 @ini_set('max_execution_time', 0);
 
 define('DOING_CRON', true);
+$included = false;
 
-include_once __DIR__.'/../../../../wp-load.php';
+if ( file_exists( __DIR__.'/../../../../wp-load.php' ) ) {
+	include_once __DIR__ . '/../../../../wp-load.php';
+	$included = true;
+} else if ( file_exists( __DIR__.'/../../../../wp-config.php' ) ) {
+	$wp_config = file_get_contents( __DIR__.'/../../../../wp-config.php' );
+	preg_match_all('/.*define[^d].*ABSPATH.*/i', $wp_config, $matches);
+	if ( count( $matches ) > 0 ) {
+		foreach ( $matches as $match ) {
+			$execute = str_ireplace( 'ABSPATH', 'TMPABSPATH', $match[0] );
+			$execute = str_ireplace( '__FILE__', "'" . __DIR__.'/../../../../wp-config.php' . "'", $execute );
+			eval( $execute );
+			if ( file_exists(TMPABSPATH . 'wp-load.php' ) ) {
+				include_once TMPABSPATH . 'wp-load.php';
+				$included = true;
+				break;
+			}
+		}
+	}
+}
+
+if ( !$included ) {
+	exit( 'Unsupported wordpress setup' );
+}

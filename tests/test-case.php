f15fbc0e6a05e0c91539ad5b333369747abf2b41
Dennis Dornon <dennisdornon@users.noreply.github.com>
2020-02-26 22:05:07 -0700

    Update test-case.php

--- tests/test-case.php
+++ tests/test-case.php
@@ -12,7 +12,7 @@ class TestCase extends MainWP_Unit_Test_Case {
 	 * Emulate deactivating, then subsequently reactivating the plugin.
 	 */
 	protected static function reactivate_plugin() {
-		$plugin = basename( dirname( __DIR__ ) ) . 'mainwp.php';
+		$plugin = basename( dirname( __DIR__ ) ) . '/mainwp.php';
 
 		do_action( 'deactivate_' . $plugin, false );
 		do_action( 'activate_' . $plugin, false );

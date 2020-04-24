ddc135417ffcfdf7aa1898c7f1bfb73b8dd9126c
Keith Crain <keith@kronoslabs.io>
2020-04-24 03:21:04 -0400

    phpDoc-Documentation

--- class/class-mainwp-live-report-responder-ativator.php
+++ class/class-mainwp-live-report-responder-ativator.php
@@ -10,18 +10,49 @@ namespace MainWP\Dashboard;
  * Class MainWP_Live_Report_Responder_Activator
  *
  * @deprecated moved to external Extension.
- *  phpcs:disable PSR1.Classes.ClassDeclaration,Generic.Files.OneObjectStructurePerFile,WordPress.DB.RestrictedFunctions, WordPress.DB.PreparedSQL.NotPrepared -- unprepared SQL ok, accessing the database directly to custom database functions - Deprecated
+ * 
+ * phpcs:disable PSR1.Classes.ClassDeclaration,Generic.Files.OneObjectStructurePerFile,WordPress.DB.RestrictedFunctions, WordPress.DB.PreparedSQL.NotPrepared -- unprepared SQL ok, accessing the database directly to custom database functions - Deprecated
  */
 class MainWP_Live_Report_Responder_Activator {
 
+	/**
+	 * @var boolean Check if MainWP is enabled.
+	 */
 	protected $mainwpMainActivated = false;
+
+	/**
+	 * @var boolean $childEnabled Check if MainWP Child plugin is enabled.
+	 */
 	protected $childEnabled        = false;
+
+	/**
+	 * @var boolean $childkey Child Site Key, false by default.
+	 */
 	protected $childKey            = false;
+
+	/**
+	 * @var undefined Child File.
+	 */
 	protected $childFile;
+	
+	/**
+	 * @var string $plugin_handle Etension Handle.
+	 */
 	protected $plugin_handle    = 'mainwp-client-reports-extension';
+
+	/**
+	 * @var string $produc_id Extention Name.
+	 */
 	protected $product_id       = 'Managed Client Reports Responder';
+
+	/**
+	 * @var string $software_version Extension version.
+	 */
 	protected $software_version = '1.1';
 
+	/**
+	 * Instanciate Plugin.
+	 */
 	public function __construct() {
 
 		$this->childFile           = __FILE__;
@@ -34,6 +65,9 @@ class MainWP_Live_Report_Responder_Activator {
 		}
 	}
 
+	/**
+	 * Activate Plugin.
+	 */
 	public function activate_this_plugin() {
 
 		$this->mainwpMainActivated = apply_filters( 'mainwp_activated_check', $this->mainwpMainActivated );
@@ -46,20 +80,35 @@ class MainWP_Live_Report_Responder_Activator {
 		new MainWP_Live_Report_Responder();
 	}
 
+	/**
+	 * Get Child Key.
+	 * 
+	 * @return mixed Child Key.
+	 */
 	public function get_child_key() {
 
 		return $this->childKey;
 	}
 
+	/**
+	 * Get Child File.
+	 * 
+	 * @return mixed Child File.
+	 */
 	public function get_child_file() {
 
 		return $this->childFile;
 	}
 
-
+	/**
+	 * Activate Plugin.
+	 */
 	public function activate() {
 	}
 
+	/**
+	 * Deactivate Plugin.
+	 */
 	public function deactivate() {
 	}
 

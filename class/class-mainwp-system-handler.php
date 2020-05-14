8b443e22f348c40f9c5f26939bc81fd2481ae49f
Bogdan Rapaić <bogdan@mainwp.com>
2020-05-14 18:41:27 +0200

    Fixed: CodeFactor issues

--- class/class-mainwp-system-handler.php
+++ class/class-mainwp-system-handler.php
@@ -15,12 +15,16 @@ class MainWP_System_Handler {
 	// phpcs:disable WordPress.WP.AlternativeFunctions -- use system functions
 
 	/**
-	 * @var $instance The single instance of the class
+	 * @var null $instance
+	 *
+	 * The single instance of the class.
 	 */
 	private static $instance = null;
 
 	/**
-	 * @var null $upgradeVersionInfo Upgrade Version Info.
+	 * @var null $upgradeVersionInfo
+	 *
+	 * Upgrade Version Info.
 	 */
 	private $upgradeVersionInfo = null;
 

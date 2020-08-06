439f12d66936ae828b750893fe5d92b67aa9e4b0
codefactor-io <support@codefactor.io>
2020-08-06 08:48:49 +0000

    [CodeFactor] Apply fixes to commit afbc3ff
    [ci skip] [skip ci]

--- class/class-mainwp-system.php
+++ class/class-mainwp-system.php
@@ -707,7 +707,7 @@ class MainWP_System {
 
 		global $wp_version;
 		if ( version_compare( $wp_version, '5.5', '>=' ) ) {
-			wp_enqueue_script( 'jquery-migrate', MAINWP_PLUGIN_URL . 'assets/js/jquery-migrate.min.js', array(), $this->current_version ); 
+			wp_enqueue_script( 'jquery-migrate', MAINWP_PLUGIN_URL . 'assets/js/jquery-migrate.min.js', array(), $this->current_version );
 		}
 
 		if ( $load_cust_scripts ) {

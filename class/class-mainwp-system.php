d09e18a1ef8e25b2164cefe41f910d01e50f8f38
thanghv <thang@mainwp.com>
2020-10-09 22:37:57 +0700

    Update version number

--- class/class-mainwp-system.php
+++ class/class-mainwp-system.php
@@ -33,7 +33,7 @@ class MainWP_System {
 	 *
 	 * @var string Current plugin version.
 	 */
-	public static $version = '4.1.2';
+	public static $version = '4.1.2.1';
 
 	/**
 	 * Private static variable to hold the single instance of the class.

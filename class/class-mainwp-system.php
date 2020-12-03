ef1ad3e7980913e6e390b105145016b48b5c85da
thanghv <thang@mainwp.com>
2020-12-03 19:04:25 +0700

    update version number

--- class/class-mainwp-system.php
+++ class/class-mainwp-system.php
@@ -33,7 +33,7 @@ class MainWP_System {
 	 *
 	 * @var string Current plugin version.
 	 */
-	public static $version = '4.1.3-beta1';
+	public static $version = '4.1.3';
 
 	/**
 	 * Private static variable to hold the single instance of the class.
--- mainwp.php
+++ mainwp.php
@@ -8,7 +8,7 @@
  * Author URI: https://mainwp.com
  * Plugin URI: https://mainwp.com/
  * Text Domain: mainwp
- * Version:  4.1.3-beta1
+ * Version:  4.1.3
  *
  * @package MainWP/Dashboard
  *

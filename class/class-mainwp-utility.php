e102cb6a2ed3cd85ba4634fb5efbc382fd4932c5
codefactor-io <support@codefactor.io>
2024-03-15 10:39:24 +0000

    [CodeFactor] Apply fixes to commit b7d9f8a
    [ci skip] [skip ci]

--- class/class-mainwp-utility.php
+++ class/class-mainwp-utility.php
@@ -580,10 +580,10 @@ class MainWP_Utility {
 	 */
 	public static function remove_http_www_prefix( $pUrl ) {
 		$pUrl = self::remove_http_prefix( $pUrl, true );
-                if(str_starts_with(strtolower($pUrl),'www.')){
-                   return substr($pUrl,4);
-                }
-                return $pUrl;
+		if ( str_starts_with(strtolower($pUrl), 'www.') ) {
+			return substr($pUrl, 4);
+		}
+				return $pUrl;
 	}
 
 	/**

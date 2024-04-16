bc68f0030e870a9e524b553d13ca15be2d778172
thanghv <thang@mainwp.com>
2024-04-16 18:53:36 +0700

    Fixed: codefactor and sonar issues

--- class/class-mainwp-system.php
+++ class/class-mainwp-system.php
@@ -22,12 +22,14 @@ const MAINWP_VIEW_PER_GROUP        = 2;
  */
 class MainWP_System {
 
+	// BEGIN-NOSCAN
 	/**
 	 * Public static variable to hold the current plugin version.
 	 *
 	 * @var string Current plugin version.
 	 */
 	public static $version = '5.0.3.1';
+	// END-NOSCAN
 
 	/**
 	 * Private static variable to hold the single instance of the class.

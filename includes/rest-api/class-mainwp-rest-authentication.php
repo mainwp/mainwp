b47a6d120ce4eba713e531e4e12b570e1914d6f2
codefactor-io <support@codefactor.io>
2024-10-11 08:56:01 +0000

    [CodeFactor] Apply fixes to commit 726da08
    [ci skip] [skip ci]

--- includes/rest-api/class-mainwp-rest-authentication.php
+++ includes/rest-api/class-mainwp-rest-authentication.php
@@ -731,7 +731,7 @@ class MainWP_REST_Authentication { //phpcs:ignore -- NOSONAR - maximumMethodThre
             return false;
         }
         // phpcs:disable WordPress.Security.NonceVerification
-        $pass = ! empty( $_REQUEST['key_pass'] ) ? wp_unslash( $_REQUEST['key_pass'] ) : ''; 
+        $pass = ! empty( $_REQUEST['key_pass'] ) ? wp_unslash( $_REQUEST['key_pass'] ) : '';
         // phpcs:enable WordPress.Security.NonceVerification
         if ( 1 === (int) $user->key_type && $pass !== $user->key_pass ) {
             $this->set_error( new \WP_Error( 'mainwp_rest_authentication_invalid_key_pass', __( 'The REST API passphrase is invalid.', 'mainwp' ), array( 'status' => 401 ) ) );

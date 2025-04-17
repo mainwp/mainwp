ebf2c089a5ff94641c015ffde11a44f33b5efa06
thanghv <thang@mainwp.com>
2025-04-18 00:46:08 +0700

    Fixed: Site id display when update WP success

--- pages/page-mainwp-updates-handler.php
+++ pages/page-mainwp-updates-handler.php
@@ -55,8 +55,10 @@ class MainWP_Updates_Handler { // phpcs:ignore Generic.Classes.OpeningBraceSameL
                     MainWP_DB::instance()->update_website_option( $website, 'wp_upgrades', wp_json_encode( array() ) );
                     do_action( 'mainwp_after_upgrade_wp_success', $website, $information );
                     // Get Icon for the largest change scope of ext regression testing.
-                    $html_regression_icon = apply_filters( 'mainwp_html_regression_largest_change_scope', $website->id, true ) ?: '';  // phpcs:ignore -- NOSONAR 
-
+                    $html_regression_icon = apply_filters( 'mainwp_html_regression_largest_change_scope', $website->id, true ) ?: '';  // phpcs:ignore -- NOSONAR
+                    if ( $website->id === $html_regression_icon ) {
+                        $html_regression_icon = '';
+                    }
                     return '<i class="green check icon"></i>' . $html_regression_icon;
                 } elseif ( isset( $information['upgrade'] ) ) {
                     $errorMsg = '';
